c3ba8c45af5f44df50421c67734cdafe39c0a577
Akito <the@akito.ooo>
2022-02-20 05:12:42 +0100

    Initialise Database

--- src/steamreviewessentialiser/database.nim
+++ src/steamreviewessentialiser/database.nim
@@ -1,10 +1,13 @@
 import
   std/[
-    sets
+    sets,
+    json
   ],
   pkg/[
     nimdbx
   ]
+
+{.experimental: "notnil".}
 
 let
   loc = "database"
@@ -13,9 +16,25 @@ let
     Exclusive,
     SafeNoSync
   }
+  collFlags = { CreateCollection }
+  collKeyType = StringKeys
+  collValType = StringValues
 
-var
-  db: Database
+var db: Database
 
-loc.eraseDatabase()
-db = loc.openDatabase(flags = flags)
+# General
+proc initDb() = db = loc.openDatabase(flags = flags)
+proc getRefClt(name: string): Collection not nil = db.openCollection(name, collFlags, collKeyType, collValType)
+
+# Write
+proc begin(clt: Collection not nil): CollectionTransaction = clt.beginTransaction()
+proc save(ct: CollectionTransaction, key, val: string) = ct.put(key, val)
+proc commit(ct: CollectionTransaction) = ct.commit
+proc abort(ct: CollectionTransaction) = ct.abort
+# Read
+proc beginShow(clt: Collection not nil): CollectionSnapshot = clt.beginSnapshot()
+proc load(snap: CollectionSnapshot, key: string): string = snap.get(key.asData).asString() # `asString` creates a copy, which survives the transaction.
+proc finishShow(snap: CollectionSnapshot) = snap.finish
+
+# General
+proc closeDb() = db.close
