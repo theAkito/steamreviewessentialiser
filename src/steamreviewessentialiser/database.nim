4d2d2102e407cbe36b114c20e422a60d7699f042
Akito <the@akito.ooo>
2022-02-21 23:55:57 +0100

    Export Database Utilities

--- src/steamreviewessentialiser/database.nim
+++ src/steamreviewessentialiser/database.nim
@@ -23,18 +23,18 @@ let
 var db: Database
 
 # General
-proc initDb() = db = loc.openDatabase(flags = flags)
-proc getRefClt(name: string): Collection not nil = db.openCollection(name, collFlags, collKeyType, collValType)
+proc initDb*() = db = loc.openDatabase(flags = flags)
+proc getRefClt*(name: string): Collection not nil = db.openCollection(name, collFlags, collKeyType, collValType)
 
 # Write
-proc begin(clt: Collection not nil): CollectionTransaction = clt.beginTransaction()
-proc save(ct: CollectionTransaction, key, val: string) = ct.put(key, val)
-proc commit(ct: CollectionTransaction) = ct.commit
-proc abort(ct: CollectionTransaction) = ct.abort
+proc begin*(clt: Collection not nil): CollectionTransaction = clt.beginTransaction()
+proc save*(ct: CollectionTransaction, key, val: string) = ct.put(key, val)
+proc commit*(ct: CollectionTransaction) = ct.commit
+proc abort*(ct: CollectionTransaction) = ct.abort
 # Read
-proc beginShow(clt: Collection not nil): CollectionSnapshot = clt.beginSnapshot()
-proc load(snap: CollectionSnapshot, key: string): string = snap.get(key.asData).asString() # `asString` creates a copy, which survives the transaction.
-proc finishShow(snap: CollectionSnapshot) = snap.finish
+proc beginShow*(clt: Collection not nil): CollectionSnapshot = clt.beginSnapshot()
+proc load*(snap: CollectionSnapshot, key: string): string = snap.get(key.asData).asString() # `asString` creates a copy, which survives the transaction.
+proc finishShow*(snap: CollectionSnapshot) = snap.finish
 
 # General
-proc closeDb() = db.close
+proc closeDb*() = db.close
