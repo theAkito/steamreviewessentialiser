f0f54737d5a037d83947668e2a333831383f1eb7
Akito <the@akito.ooo>
2022-02-25 19:45:58 +0100

    Fix Shared Except Messages

--- src/steamreviewessentialiser/apiutils.nim
+++ src/steamreviewessentialiser/apiutils.nim
@@ -3,8 +3,8 @@ import
 from puppy import Header
 
 const
-  exceptMsgMsgPostErrorParse* = "Unable to post a message due to JSON parsing error!"
-  exceptMsgMsgPostErrorAPI* = "Unable to post a message due to an API error!"
+  exceptMsgMsgPostErrorParse* = "Unable to read revuew due to JSON parsing error!"
+  exceptMsgMsgPostErrorAPI* = "Unable to retrieve review due to an API error!"
   headerJson* = Header(key: "Content-type", value: "application/json")
 
 func is20x*(code: int): bool = code.intToStr().startsWith("20")
