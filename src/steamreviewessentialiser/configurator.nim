05ca350675253c51cc1e8ace41fd8dcf08d66523
Akito <the@akito.ooo>
2022-02-25 19:13:46 +0100

    Add Configurator

---
+++ src/steamreviewessentialiser/configurator.nim
@@ -0,0 +1,24 @@
+import
+  meta,
+  json,
+  base64,
+  os,
+  logging,
+  strutils
+
+type
+  MasterConfig = object
+    version                   *: string
+    maxItems                  *: int64
+    intervalAPI               *: int
+    debug                     *: bool
+
+let
+  jNodeEmpty = newJObject()
+  logger = newConsoleLogger(defineLogLevel(), logMsgPrefix & logMsgInter & "configurator" & logMsgSuffix)
+  config* = MasterConfig(
+    version: appVersion,
+    intervalAPI: 10_000,
+    maxItems: 500,
+    debug: meta.debug
+  )
