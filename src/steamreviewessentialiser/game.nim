27e140a28a1922bdd314f9ee1ee6f7378c21a9fc
Akito <the@akito.ooo>
2022-02-27 23:20:15 +0100

    Getting Started with Game Module

--- src/steamreviewessentialiser/game.nim
+++ src/steamreviewessentialiser/game.nim
@@ -2,3 +2,15 @@
   Retrieve information about Steam games.
   Mainly needed for searching a game through human titles and then matching their unique App IDs.
 ]##
+
+import
+  meta,
+  std/[
+    re
+  ],
+  pkg/[
+    puppy
+  ]
+
+const
+  url = "https://api.steampowered.com/ISteamApps/GetAppList/v0002"
