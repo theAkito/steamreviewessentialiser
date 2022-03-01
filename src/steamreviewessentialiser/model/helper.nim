5a96e3d2e9766bf03342d84f2b8b11b4cdf5c534
Akito <the@akito.ooo>
2022-03-02 00:51:15 +0100

    Add ClientType for ApiRequest

--- src/steamreviewessentialiser/model/helper.nim
+++ src/steamreviewessentialiser/model/helper.nim
@@ -9,7 +9,11 @@ type
     all = "all",
     non_steam_purchase = "non_steam_purchase",
     steam = "steam" 
-  Language * = enum
-    ## https://partner.steamgames.com/doc/store/localization
+  Language * = enum ## https://partner.steamgames.com/doc/store/localization
     english = "english",
     all = "all"
+
+  ClientType * = enum ## Type of client sending the Tag Cloud request to this server.
+    desktop = "desktop",
+    mobile = "mobile",
+    webui = "webui"
