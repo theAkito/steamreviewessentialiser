111c55843fa5a45a2efb9294d43fe63087256e07
Akito <the@akito.ooo>
2022-03-02 03:17:20 +0100

    Initialise Request Processor

--- src/steamreviewessentialiser/requestprocessor.nim
+++ src/steamreviewessentialiser/requestprocessor.nim
@@ -2,6 +2,8 @@
   Process requests coming from clients.
 ]##
 
+# https://github.com/dom96/jester#custom-router
+
 import
   meta,
   apiutils,
@@ -14,5 +16,16 @@ import
     strutils
   ],
   pkg/[
-    puppy
+    jester
   ]
+
+router requestprocessor:
+  post "/api":
+    resp pretty(request.body.parseJson), rawHeaderJson
+
+proc listen*() =
+  let
+    port = 1337.Port
+    settings = newSettings(port)
+  var srv = initJester(requestprocessor, settings)
+  srv.serve
