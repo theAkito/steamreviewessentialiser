042465ac02718e91b803b674b69b48c364826a55
Akito <the@akito.ooo>
2022-03-03 00:23:06 +0100

    Apply Spell Correction & Word Root Extraction to Cloud Tag Generator

--- src/steamreviewessentialiser/cloudor.nim
+++ src/steamreviewessentialiser/cloudor.nim
@@ -4,17 +4,46 @@
 
 import
   meta,
+  spellor,
   model/[
     tag
   ],
   std/[
     json,
     logging,
-    strutils
+    strutils,
+    sequtils,
+    tables,
+    sets
   ]
 
+let logger = newConsoleLogger(defineLogLevel(), logMsgPrefix & logMsgInter & "cloudor" & logMsgSuffix)
+
+func extractWords(text: openArray[string]): seq[string] =
+  for content in text:
+    for word in content.split():
+      result.add word
+
+func mapTagToPopularity(words: openArray[string], amount: int = words.len): OrderedTable[string, int] =
+  var
+    counter: int
+    countTable = words.toCountTable
+  countTable.sort
+  for word, freq in countTable.pairs:
+    counter.inc
+    result[word] = freq
+    if counter >= amount: return
+
+func extractMostUsed*(text: openArray[string], amount: int = 15): OrderedTable[string, int] =
+  text.extractWords.mapTagToPopularity(amount)
+
+proc extractMostUsedRoots*(text: openArray[string], amount: int = 15): OrderedTable[string, int] =
+  for word, freq in text.extractMostUsed(amount).pairs:
+    result[word.root] = freq
+  logger.log(lvlDebug, "[extractMostUsedRoots] Result: " & pretty(%* result))
+
 when isMainModule:
-  import tables, sets, sequtils
+  # import tables, sets, sequtils
   const testReviewContent = ["mostused aint what for lobby", "lobby why mostused", "mostused aint what"]
   var
     words: seq[string]
