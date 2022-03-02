7df331f16fdc5c66c4b096dd66f86cbd25635960
Akito <the@akito.ooo>
2022-03-02 03:16:51 +0100

    Add API Utility

--- src/steamreviewessentialiser/apiutils.nim
+++ src/steamreviewessentialiser/apiutils.nim
@@ -6,5 +6,6 @@ const
   exceptMsgMsgPostErrorParse* = "Unable to read revuew due to JSON parsing error!"
   exceptMsgMsgPostErrorAPI* = "Unable to retrieve review due to an API error!"
   headerJson* = Header(key: "Content-type", value: "application/json")
+  rawHeaderJson* = "Content-Type: application/json"
 
 func is20x*(code: int): bool = code.intToStr().startsWith("20")
