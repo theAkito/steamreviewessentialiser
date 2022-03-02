faf1890044846423070b0958579f3492f77c4e71
Akito <the@akito.ooo>
2022-03-02 22:38:25 +0100

    Apply Nim bindings for the C++ STL

--- src/steamreviewessentialiser/spellor.nim
+++ src/steamreviewessentialiser/spellor.nim
@@ -2,6 +2,8 @@
   Spell checker & auto-corrector.
 ]##
 
+# https://forum.nim-lang.org/t/6833#42762
+
 import
   meta,
   model/[
@@ -12,6 +14,9 @@ import
     json,
     logging,
     strutils
+  ],
+  pkg/[
+    cppstl
   ]
 
 const header = "externlib/hunspell/src/hunspell/hunspell.hxx"
@@ -24,6 +29,11 @@ proc newHunspell(affpath: cstring, dpath: cstring): ptr HunspellObj {.importcpp:
 proc addDic(hunspell: ptr HunspellObj, dpath: cstring): cint {.header: header, importcpp: "#.add_dic(@)".}
 proc spell(hunspell: ptr HunspellObj, word: cstring): bool {.header: header, importcpp: "#.spell(@)".}
 proc spell(hunspell: ptr HunspellObj, word: cstring, info: cint, root: cstring): bool {.header: header, importcpp: "#.spell(@)".}
+proc suggest(hunspell: ptr HunspellObj, word: cstring): CppVector[CppString] {.header: header, importcpp: "#.suggest(@)".}
+# C++ Support Utilities
+func high(v: CppVector): uint = v.len - 1
+func toSeq(v: CppVector[CppString]): seq[string] =
+  for word in v: result.add word.toString
 
 const
   pathDictBase = "src/steamreviewessentialiser/externlib/dictionaries/"
@@ -35,3 +45,7 @@ var
 when isMainModule:
   echo "Spelling of \"Recommendation\" is correct: " & $hunspell.spell("Recommendation".cstring)
   echo "Spelling of \"Recommnedation\" is correct: " & $hunspell.spell("Recommnedation".cstring)
+  echo()
+  echo "Suggestions for \"Recommendation\":\n" & pretty(% hunspell.suggest("Recommendation".cstring).toSeq)
+  echo()
+  echo "Suggestions for \"hell\":\n" & pretty(% hunspell.suggest("hell".cstring).toSeq)
