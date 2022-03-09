6e1df5861f416311f17e93c7a4d2b35df053cc4a
Akito <the@akito.ooo>
2022-03-09 02:57:21 +0100

    Re-Implement Hunspell in C

--- src/steamreviewessentialiser/spellor.nim
+++ src/steamreviewessentialiser/spellor.nim
@@ -3,57 +3,70 @@
   Currently, only deals with text written in American English.
 ]##
 
-# https://forum.nim-lang.org/t/6833#42762
-
-import cppstl
-
-const header = "steamreviewessentialiser/externlib/hunspell/src/hunspell/hunspell.hxx"
+const header = when isMainModule: "externlib/hunspell/src/hunspell/hunspell.h" else: "steamreviewessentialiser/externlib/hunspell/src/hunspell/hunspell.h"
 
 type
-  HunspellObj {.header: header, importcpp: "Hunspell".} = object
+  Hunhandle {.header: header, importc: "Hunhandle".} = object
 
-# Import C++
-proc newHunspell(affpath: cstring, dpath: cstring): ptr HunspellObj {.importcpp: "new Hunspell(@)".}
-proc addDic(hunspell: ptr HunspellObj, dpath: cstring): cint {.header: header, importcpp: "#.add_dic(@)".}
-proc spell(hunspell: ptr HunspellObj, word: cstring): bool {.header: header, importcpp: "#.spell(@)".}
-proc spell(hunspell: ptr HunspellObj, word: cstring, info: cint, root: cstring): bool {.header: header, importcpp: "#.spell(@)".}
-proc suggest(hunspell: ptr HunspellObj, word: cstring): CppVector[CppString] {.header: header, importcpp: "#.suggest(@)".}
-proc analyze(hunspell: ptr HunspellObj, word: cstring): CppVector[CppString] {.header: header, importcpp: "#.analyze(@)".}
-proc stem(hunspell: ptr HunspellObj, morph: CppVector[CppString]): CppVector[CppString] {.header: header, importcpp: "#.stem(@)".}
-# C++ Support Utilities
-func high(v: CppVector): uint = v.len - 1
-func toSeq(v: CppVector[CppString]): seq[string] =
-  for word in v: result.add word.toString
+# Import C
+proc newHunspell(affpath: cstring, dpath: cstring): ptr Hunhandle {.header: header, importc: "Hunspell_create".}
+proc addDic(hunspell: ptr Hunhandle, dpath: cstring): cint {.header: header, importc: "Hunspell_add_dic".}
+proc spell(hunspell: ptr Hunhandle, word: cstring): bool {.header: header, importc: "Hunspell_spell".}
+proc suggest(hunspell: ptr Hunhandle, resultArray: ptr cstringArray, word: cstring): cint {.header: header, importc: "Hunspell_suggest".}
+proc analyze(hunspell: ptr Hunhandle, resultArray: ptr cstringArray, word: cstring): cint {.header: header, importc: "Hunspell_analyze".}
+proc stem(hunspell: ptr Hunhandle, suggestions: ptr cstringArray, morph: cstringArray, n: cint): cint {.header: header, importc: "Hunspell_stem2".}
 
 const
   pathDictBase = "src/steamreviewessentialiser/externlib/dictionaries/"
   pathAff_EN_US: cstring = pathDictBase & "en_US.aff"  ## Path to AFF file.
   pathDict_EN_US: cstring = pathDictBase & "en_US.dic" ## Path to DIC file.
 var
-  hunspell: ptr HunspellObj = newHunspell(pathAffEN_US, pathDictEN_US) ## Hunspell instance.
+  hunspell: ptr Hunhandle = newHunspell(pathAffEN_US, pathDictEN_US) ## Hunspell instance.
 
 proc spell*(word: string): bool = hunspell.spell(word.cstring)
-proc suggest*(word: string): seq[string] = hunspell.suggest(word.cstring).toSeq
+proc suggest*(word: string): seq[string] =
+  var resultSuggest: cstringArray
+  let resultSuggestLen = hunspell.suggest(resultSuggest.addr, word.cstring)
+  result = resultSuggest.cstringArrayToSeq(resultSuggestLen)
 proc stem*(word: string): string = # Sequence is empty and then excepts, due to index error, when stem cannot be detected.
-  try: hunspell.stem(hunspell.analyze(word.cstring)).toSeq()[0] except: word
+  try:
+    var
+      resultStem: cstringArray
+      resultMorph: cstringArray
+    var resultMorphLen = hunspell.analyze(resultMorph.addr, word.cstring)
+    if resultMorphLen > 0: 
+      let stemsAmountFound = hunspell.stem(resultStem.addr, resultMorph, resultMorphLen)
+      result = if stemsAmountFound > 0: $resultStem[0] else: word
+    else:
+      return word
+  except:
+    result = word
 
 proc root*(word: string): string =
   ## Get most likely correct version of word.
   ## Then extract word stem.
   if spell(word): return word.stem
-  let correct = word.suggest()[0]
+  let correct = try: word.suggest()[0] except: return word.stem
   correct.stem
 
 when isMainModule:
   import json
+  var resultArray: cstringArray
   echo "Spelling of \"Recommendation\" is correct: " & $hunspell.spell("Recommendation".cstring)
   echo "Spelling of \"Recommnedation\" is correct: " & $hunspell.spell("Recommnedation".cstring)
   echo()
-  echo "Suggestions for \"Recommendation\":\n" & pretty(% hunspell.suggest("Recommendation".cstring).toSeq)
+  echo hunspell.suggest(resultArray.addr, "Recommendation".cstring)
+  echo "Suggestions for \"Recommendation\":\n" & pretty(% $resultArray[0])
   echo()
-  echo "Suggestions for \"hell\":\n" & pretty(% hunspell.suggest("hell".cstring).toSeq)
+  resultArray = nil
+  echo hunspell.suggest(resultArray.addr, "hell".cstring)
+  echo "Suggestions for \"hell\":\n" & pretty(% $resultArray[0])
   echo()
-  let morph1 = hunspell.analyze("Mixing".cstring)
-  echo "Analysis for \"Mixing\":\n" & pretty(% morph1.toSeq)
+  resultArray = nil
+  var resultMorph: cstringArray
+  var n = hunspell.analyze(resultMorph.addr, "Mixing".cstring)
+  echo "Analysis for \"Mixing\":\n" & pretty(% $resultMorph[0])
   echo()
-  echo "Stem from morphological analysis for \"Mixing\":\n" & pretty(% hunspell.stem(morph1).toSeq)
+  resultArray = nil
+  echo hunspell.stem(resultArray.addr, resultMorph, n)
+  echo "Stem from morphological analysis for \"Mixing\":\n" & pretty(% $resultArray[0])
