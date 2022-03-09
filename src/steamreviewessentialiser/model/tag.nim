c928307ca5e387a7764cfbaea20751f4d675c2d0
Akito <the@akito.ooo>
2022-03-09 02:54:21 +0100

    Re-Work TagCloud model

--- src/steamreviewessentialiser/model/tag.nim
+++ src/steamreviewessentialiser/model/tag.nim
@@ -1,11 +1,12 @@
-from helper import Language
-from sets import
-  HashSet,
-  OrderedSet
+from helper import
+  Language
+from tables import
+  OrderedTable
+from database import
+  DatabaseConfig
 
 type
   TagCloud * = ref object
-    tags        * : HashSet[string]    ## Simple list of all tags, but without duplicates.
-    tagsPop     * : OrderedSet[string] ## Tags ordered by popularity. tagsPop.low == most popular, whereas tagsPop.high == least popular.
-    tagsLikeDup * : HashSet[string]    ## Tags, which are different technically, but probably mean the same, most likely due to spelling mistake.
-    language    * : Language           ## Human language used for tag creation.
+    tagsToPop   * : OrderedTable[string, int] ## Tags ordered by popularity. tagsPop.low == most popular, whereas tagsPop.high == least popular.
+    config      * : DatabaseConfig             ## Tags ordered by popularity. tagsPop.low == most popular, whereas tagsPop.high == least popular.
+    language    * : Language             ## Human language used for tag creation.
