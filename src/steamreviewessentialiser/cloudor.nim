948d590f0d57308dab3f99c0cf623a961db0ec68
Akito <the@akito.ooo>
2022-03-09 02:56:40 +0100

    Generate TagCloud

--- src/steamreviewessentialiser/cloudor.nim
+++ src/steamreviewessentialiser/cloudor.nim
@@ -5,8 +5,11 @@
 import
   meta,
   word,
+  filter,
+  database,
+  configurator,
   model/[
-    database,
+    steam,
     tag
   ],
   std/[
@@ -17,3 +20,22 @@ import
     tables,
     sets
   ]
+
+let logger = newConsoleLogger(defineLogLevel(), logMsgPrefix & logMsgInter & "cloudor" & logMsgSuffix)
+
+func extractReviews(reviews: seq[SteamReviewItemRes]): seq[string] = reviews.mapIt(it.review)
+func toTagCloud(mostUsed: OrderedTable[string, int]): TagCloud = TagCloud(tagsToPop: mostUsed)
+
+proc loadAllReviews(appid: string): seq[SteamReviewItemRes] =
+  for review in appid.loadAllReviews:
+    result.add review
+
+proc loadMostUsed(appid: string): OrderedTable[string, int] =
+  appid.loadAllReviews.extractReviews.extractMostUsed(config.maxTags)
+
+proc loadMostUsedRoots(appid: string): OrderedTable[string, int] =
+  appid.loadAllReviews.extractReviews.extractMostUsedRoots(config.maxTags)
+
+proc loadTagCloud*(appid: string): TagCloud =
+  result = appid.loadMostUsedRoots.toTagCloud
+  result.config = loadDatabaseConfig(appid)
