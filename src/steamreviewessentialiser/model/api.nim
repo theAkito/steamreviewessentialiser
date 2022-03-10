966377e59d6e86c900543cecc4569403b8255db2
Akito <the@akito.ooo>
2022-03-11 00:04:43 +0100

    Re-Work Component Models

--- src/steamreviewessentialiser/model/api.nim
+++ src/steamreviewessentialiser/model/api.nim
@@ -6,15 +6,25 @@ import
 type
   ApiDefect * = object of Defect
 
+  ApiRequestAdmin * = ref object
+    ## Any option provided here requires administrator permissions,
+    ## i.e. successful authentication through the provided `adminToken`.
+    token        * : string               ## Authenticate client with server administrator permissions. Base64 encoded string.
+    ifAvailable  * : Option[bool]         ## Do not gather review data and save to database, if this particular game's tag cloud data is not yet available in the database. Default: false (Will gather review data, if none is found in the database & authentication as administrator succeeded.)
+    forceFresh   * : Option[bool]         ## Force refresh of review data for this particular game, even when it has already been retrieved in the past.
+    amountReview * : Option[int]          ## Amount of reviews to retrieve, which the TagCloud generation will be based on. The more reviews are gathered, the longer it takes, but the result will be more precise on average across all possible reviews.
+    amountTag    * : Option[int]          ## Amount of tags the TagCloud should at least contain. If not enough reviews are found to generate the TagCloud from, then less tags than requested might be sent.
+    language     * : Option[Language]     ## Which human language is requested. Default: "english"
+    reviewType   * : Option[ReviewType]   ## Which review type is requested. Default: "all" (positive & negative)
+    purchaseType * : Option[PurchaseType] ## Which purchase type is requested. Default: "all" (Non-Steam & Steam)
+
   ApiRequest * = ref object
-    version    * : uint           ## API version.
-    appid      * : uint32         ## Client has to search for game by string, select the correct game and then the associated appid will be sent to this server.
-    clientType * : ClientType     ## Client type, like e.g. "desktop" or "mobile".
-    adminToken * : Option[string] ## Authenticate client with server administrator permissions. Base64 encoded string.
-    forceFresh * : Option[bool]   ## Requires administrator permissions. Force refresh of review data for this particular game, even when it has already been retrieved in the past.
-    language   * : Language       ## Requires administrator permissions. Human language used for tag creation.
+    version      * : uint                 ## API version.
+    appid        * : uint32               ## Client has to search for game by string, select the correct game and then the associated appid will be sent to this server.
+    clientType   * : ClientType           ## Client type, like e.g. "desktop" or "mobile".
+    admin        * : Option[ApiRequestAdmin]
 
   ApiResponse * = ref object
-    appid      * : uint32          ## The appid of the game the Tag Cloud is for.
-    cloud      * : TagCloud        ## Tag Cloud requested for the appid.
-    timestamp  * : string          ## Timestamp as string of the most recent review in the collection used to generate the Tag Cloud. Timestamp is retrieved from the most recent review's `timestamp_created` property.
+    appid        * : uint32               ## The appid of the game the Tag Cloud is for.
+    success      * : bool                 ## Whether the request was successful. Can fail, when the game is not found, or because gathering is interrupted, when requesting a forced refresh, etc.
+    cloud        * : TagCloud             ## Tag Cloud requested for the appid.
--- src/steamreviewessentialiser/model/tag.nim
+++ src/steamreviewessentialiser/model/tag.nim
@@ -7,6 +7,9 @@ from database import
 
 type
   TagCloud * = ref object
-    tagsToPop   * : OrderedTable[string, int] ## Tags ordered by popularity. tagsPop.low == most popular, whereas tagsPop.high == least popular.
-    config      * : DatabaseConfig             ## Tags ordered by popularity. tagsPop.low == most popular, whereas tagsPop.high == least popular.
-    language    * : Language             ## Human language used for tag creation.
+    tagsToPop         * : OrderedTable[string, int] ## Tags ordered by popularity. tagsPop.low == most popular, whereas tagsPop.high == least popular.
+    config            * : DatabaseConfig            ## Configuration for the reviews, which the provided TagCloud is generated from. Does reflect what SHOULD be the case, not what necessarily IS the case.
+    reviewAmount      * : int                       ## Amount of reviews used to generate this TagCloud from.
+    timestampLatest   * : string                    ## Timestamp as string of the most recent review in the collection used to generate the Tag Cloud. Timestamp is retrieved from the most recent review's `timestamp_created` property.
+    timestampUpdate   * : string                    ## When was the last time the collection was refreshed with updated versions of reviews.
+    timestampComplete * : string                    ## When was the last time the collection finished gathering the amount of requested reviews for this particular game.
