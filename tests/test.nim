33fe1ceeef95283c83a3a667a4c050257dc3ec4f
Akito <the@akito.ooo>
2022-03-21 21:52:14 +0100

    Re-Vamp Simple Request Tester

--- tests/test.nim
+++ tests/test.nim
@@ -1,18 +1,47 @@
 import
   meta,
-  json,
-  database,
   logging,
-  nimdbx,
   simplerequest,
   os,
-  osproc,
+  parseopt,
   strutils
 
 let logger = newConsoleLogger(lvlDebug, logMsgPrefix & logMsgInter & "tester" & logMsgSuffix)
 
+var
+  srvStart = false
+  forceFresh = false
+
+proc showHelp() =
+  logger.log(lvlWarn, """Arguments available:""")
+  logger.log(lvlWarn, """srv-start""")
+  logger.log(lvlWarn, """force-fresh""")
+
+template breakOutOpts(): untyped =
+  if not val.isEmptyOrWhitespace(): showHelp()
+
+proc setOpts() =
+  for kind, key, val in commandLineParams().getopt():
+    case kind
+      of cmdArgument:
+        # Use `key` to get value of `cmdArgument`.
+        showHelp()
+      of cmdLongOption, cmdShortOption:
+        case key
+          of "s", "srv-start":
+            breakOutOpts()
+            srvStart = true
+          of "f", "force-fresh":
+            breakOutOpts()
+            forceFresh = true
+          of "h", "help":
+            showHelp()
+      of cmdEnd: assert(false)
+
+setOpts()
+
 makeRequest(
-  srvStart = try: commandLineParams()[0].parseBool except: false,
-  forceFresh = try: commandLineParams()[1].parseBool except: false
+  srvStart = srvStart,
+  forceFresh = forceFresh
 )
 
